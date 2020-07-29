f603e2f0cfda8a971bd104f1902064fc6d345b65
Hashim Gari <hashmaster3k@gmail.com>
2020-07-29 09:04:38 -0600

    Adds vendor class and test file

---
+++ test/vendor_test.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+require './lib/vendor'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class VendorTest < Minitest::Test
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_instance_of Vendor, vendor
+  end
+end
