50327213197aa495af050917d528e6ca59ea8f46
Hashim Gari <hashmaster3k@gmail.com>
2020-07-29 09:41:28 -0600

    Adds ability to calculate potential revenue

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -14,4 +14,12 @@ class Vendor
   def stock(item, amount)
     @inventory[item] += amount
   end
+
+  def potential_revenue
+    total = 0
+    @inventory.each do |item|
+      total += item[0].price * item[1]
+    end
+    total
+  end
 end
