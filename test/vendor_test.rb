229133ae17d5b130f7a426d792fe7d30da399b7c
Hashim Gari <hashmaster3k@gmail.com>
2020-07-29 09:05:59 -0600

    Adds readable attributes

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -8,4 +8,10 @@ class VendorTest < Minitest::Test
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_instance_of Vendor, vendor
   end
+
+  def test_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal [], vendor.inventory
+  end
 end
